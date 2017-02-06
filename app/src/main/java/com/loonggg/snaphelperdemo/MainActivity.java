97700df71f0b8caf65e89497b23dbcc3b0e4b9c6
loonggg <loonggg@qq.com>
2017-02-06 19:52:30 +0800

    image

--- app/src/main/java/com/loonggg/snaphelperdemo/MainActivity.java
+++ app/src/main/java/com/loonggg/snaphelperdemo/MainActivity.java
@@ -3,6 +3,7 @@ package com.loonggg.snaphelperdemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.LinearSnapHelper;
 import android.support.v7.widget.RecyclerView;
 
 import java.util.ArrayList;
